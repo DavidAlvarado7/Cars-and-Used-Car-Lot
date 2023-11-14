9d5f8defb422b2487fe3425bf618ff16bf645aff
DavidAlvarado7 <144187066+DavidAlvarado7@users.noreply.github.com>
2023-11-13 19:13:20 -0500

    Update not Finished

---
+++ Cars-and-Used-Car-Lot/Car.cs
@@ -0,0 +1,73 @@
+﻿using System;
+namespace Cars_and_Used_Car_Lot
+{
+	public class Car
+	{
+		public string Make { get; set; }
+
+		public string Model { get; set; }
+
+		public int Year{ get; set;  }
+
+		public decimal Price{ get; set; }
+
+		public Car()
+		{
+			Make = string.Empty;
+			Model = string.Empty;
+			Year = 0;
+			Price = 0;
+
+
+		}
+
+		public Car(string make, string model, int year, decimal price)
+		{
+			Make = make;
+			Model = model;
+			Year = year;
+			Price = price;
+
+
+
+		}
+
+        public override string ToString()
+        {
+            return $"{Make}    {Model}	 Year: {Year}	price: ${Price}"; 
+
+        }
+    }
+
+
+	public class UsedCars : Car
+	{
+
+		public double Mileage { get; set; }
+
+
+		public UsedCars(string make, string model, int year, decimal price, double mileage)
+		{
+			Make = make;
+			Model = model;
+			Year = year;
+			Price = price;
+			Mileage = mileage;
+
+			
+
+		}
+
+        public override string ToString()
+        { 
+			return $"{Make}    {Model}  Year: {Year}  Price:   ${Price}      Mileage: {Mileage}";
+        }
+
+    }
+
+
+	
+
+		
+}
+
--- Cars-and-Used-Car-Lot/Program.cs
+++ Cars-and-Used-Car-Lot/Program.cs
@@ -2,9 +2,72 @@
 
 class Program
 {
+    
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Hello which vehicle would you like to buy?..");
+
+        
+
+
+
+
+
+
+
+        var newCars = new List<Car>();
+        var usedCars = new List<UsedCars>();
+        var car = new Car("ford", "Mustang", 2023, 45_000);
+        var car2 = new Car("Hyundai", "Elantra", 2022, 32_600);
+        var car3 = new Car("Chrysler", "200", 2012, 35_000);
+        var usedCar1 = new UsedCars("Cheverlot", "Custom", 2015, 12_000, 150_000);
+        var usedCar2 = new UsedCars("Toyota", "Sianna", 2015, 10_000, 80_000);
+        var usedCar3 = new UsedCars("Nissan", "Sentra", 2010, 6_000, 90_000);
+        newCars.Add(car);
+        newCars.Add(car2);
+        newCars.Add(car3);
+        usedCars.Add(usedCar1);
+        usedCars.Add(usedCar2);
+        usedCars.Add(usedCar3);
+        var userChoice = Console.ReadLine();
+        if(int.TryParse(userChoice, out int choice))
+        {
+           
+
+
+        }
+        
+        ListCars(newCars, usedCars);
+        
     }
+
+    static void ListCars(List<Car> Cars, List<UsedCars> usedCars)
+    {
+        for (int x = 0; x < Cars.Count; x += 3)
+        {
+            var car1 = Cars[x + 0];
+            var car2 = Cars[x + 1];
+            var car3 = Cars[x + 2];
+            Console.WriteLine($"We have \nNew: {car1} \nNew: {car2}\nNew: {car3}");
+
+        }
+        for (int i = 0; i < usedCars.Count; i += 3)
+        {
+            var usedCar = usedCars[i + 0];
+            var usedCar2 = usedCars[i + 1];
+            var usedCar3 = usedCars[i + 2];
+            Console.WriteLine($"Used: {usedCar} \nUsed: {usedCar2} \nUsed: {usedCar3}");
+        }
+
+    }
+
+     static void remove(List<Car> removeCar)
+    {
+        removeCar = new List<Car>();
+
+    }
+
 }
+
 
